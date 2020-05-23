9e596cbc2fa55b7f7b7855e3a62f24b1dba44d7d
Martin Janys <martinjanys@gmail.com>
2020-05-23 23:08:45 +0200

    option window

--- src/options/components/SettingsForm.vue
+++ src/options/components/SettingsForm.vue
@@ -2,7 +2,7 @@
   <div class="row">
     <form class="col s12">
       <div class="row">
-        <div class="input-field col s6">
+        <div class="input-field col s12">
           <input
               v-model="accessToken"
               id="access_token"
@@ -10,9 +10,13 @@
               placeholder="Access token">
           <label for="access_token">Github Access Token</label>
         </div>
+      </div>
+      <div class="row">
+        <div class="col s12">
         <a href="https://github.com/settings/tokens/new?scopes=repo&description=floating-dots">
-          Github Access Tokens
-        </a>
+            Github Access Tokens
+          </a>
+        </div>
       </div>
     </form>
   </div>
