1000d634ef7ac6d8d8d9f8fc6cd44abb771f324d
Martin Janys <martinjanys@gmail.com>
2020-06-02 20:54:24 +0200

    reverseElements fix

--- src/inject/functions/reverseElements.js
+++ src/inject/functions/reverseElements.js
@@ -1,5 +1,7 @@
 export default function(parent) {
-  for (let i = 1; i < parent.childNodes.length; i++){
-    parent.insertBefore(parent.childNodes[i], parent.firstChild);
+  if (parent) {
+    for (let i = 1; i < parent.childNodes.length; i++) {
+      parent.insertBefore(parent.childNodes[i], parent.firstChild);
+    }
   }
 }
